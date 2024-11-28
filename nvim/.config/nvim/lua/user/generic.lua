82745abcb2bc2e7b1d63e9f2a31b22c4f4e7c58b
Luca Vaccari <luca.vaccari@proton.me>
2024-11-28 21:15:05 +0100

    Nvim generic

--- nvim/.config/nvim/lua/user/generic.lua
+++ nvim/.config/nvim/lua/user/generic.lua
@@ -1,11 +1,8 @@
 -- GENERIC OPTIONS
-vim.opt.number = true -- show line numbers
+vim.opt.number = true    -- show line numbers
 vim.opt.relativenumber = true
 vim.opt.smartcase = true -- search for uppercase only if search word contains uppercase
-vim.opt.hlsearch = false -- disable search highlight after search is done
-vim.opt.tabstop = 4 -- tab 4 spaces
-vim.opt.shiftwidth = 4 -- shift 4 spaces when << or >>
-
-vim.g.mapleader = " " -- leader key
-
-
+vim.opt.tabstop = 4      -- tab 4 spaces
+vim.opt.shiftwidth = 4   -- shift 4 spaces when << or >>
+vim.opt.cursorline = true
+vim.g.mapleader = " "    -- leader key
