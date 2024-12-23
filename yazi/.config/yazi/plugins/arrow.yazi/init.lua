38c967c80b1e7d79e7c8387beb09df8151266535
Luca Vaccari <luca.vaccari@proton.me>
2024-12-23 07:35:53 +0100

    Reaper new plugins

--- yazi/.config/yazi/plugins/arrow.yazi/init.lua
+++ yazi/.config/yazi/plugins/arrow.yazi/init.lua
@@ -2,7 +2,7 @@
 return {
 	entry = function(_, args)
 		local current = cx.active.current
-		local new = (current.cursor + args[1]) % #current.files
+		local new = (current.cursor + job.args[1]) % #current.files
 		ya.manager_emit("arrow", { new - current.cursor })
 	end,
 }
